095e08e80e7d41177e6f6422bed37ef6fb64dbc8
atscfpoi <15338133499@163.com>
2024-08-25 00:58:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,56 @@
 import requests, json, re, os
 
 session = requests.session()
-# 机场的地址
-url = os.environ.get('URL')
 # 配置用户名（一般是邮箱）
-
-config = os.environ.get('CONFIG')
+email = os.environ.get('EMAIL')
+# 配置用户名对应的密码 和上面的email对应上
+passwd = os.environ.get('PASSWD')
 # server酱
 SCKEY = os.environ.get('SCKEY')
+# PUSHPLUS
+Token = os.environ.get('TOKEN')
+def push(content):
+    if SCKEY != '1':
+        url = "https://sctapi.ftqq.com/{}.send?title={}&desp={}".format(SCKEY, 'ikuuu签到', content)
+        requests.post(url)
+        print('推送完成')
+    elif Token != '1':
+        headers = {'Content-Type': 'application/json'}
+        json = {"token": Token, 'title': 'cat签到', 'content': content, "template": "json"}
+        resp = requests.post(f'http://www.pushplus.plus/send', json=json, headers=headers).json()
+        print('push+推送成功' if resp['code'] == 200 else 'push+推送失败')
+    else:
+        print('未使用消息推送推送！')
 
-login_url = '{}/auth/login'.format(url)
-check_url = '{}/user/checkin'.format(url)
+# 会不定时更新域名，记得Sync fork
 
-def sign(order,user,pwd):
-        global url,SEKEY
-        header = {
-        'origin': url,
+login_url = 'https://speedcat.co/auth/login'
+check_url = 'https://speedcat.co/user/checkin'
+info_url = 'https://speedcat.co/user/profile'
+
+header = {
+        'origin': 'https://speedcat.co',
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-        }
-        data = {
-        'email': user,
-        'passwd': pwd
-        }
-        try:
-                print(f'===账号{order}进行登录...===')
-                print(f'账号：{user}')
-                response = json.loads(session.post(url=login_url,headers=header,data=data).text)
-                print(response['msg'])
-                # 进行签到
-                result = json.loads(session.post(url=check_url,headers=header).text)
-                print(result['msg'])
-                content = result['msg']
-                # 进行推送
-                if SCKEY != '':
-                        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
-                        requests.post(url=push_url)
-                        print('推送成功')
-        except:
-                content = '签到失败'
-                print(content)
-                if SCKEY != '':
-                        push_url = 'https://sctapi.ftqq.com/{}.send?title=机场签到&desp={}'.format(SCKEY, content)
-                        requests.post(url=push_url)
-                        print('推送成功')
-        print('===账号{order}签到结束===\n'.format(order=order))
-if __name__ == '__main__':
-        configs = config.splitlines()
-        if len(configs) %2 != 0 or len(configs) == 0:
-                print('配置文件格式错误')
-                exit()
-        user_quantity = len(configs)
-        user_quantity = user_quantity // 2
-        for i in range(user_quantity):
-                user = configs[i*2]
-                pwd = configs[i*2+1]
-                sign(i,user,pwd)
-        
+}
+data = {
+        'email': email,
+        'passwd': passwd
+}
+try:
+    print('进行登录...')
+    response = json.loads(session.post(url=login_url,headers=header,data=data).text)
+    print(response['msg'])
+    # 获取账号名称
+    info_html = session.get(url=info_url,headers=header).text
+#     info = "".join(re.findall('<span class="user-name text-bold-600">(.*?)</span>', info_html, re.S))
+#     print(info)
+    # 进行签到
+    result = json.loads(session.post(url=check_url,headers=header).text)
+    print(result['msg'])
+    content = result['msg']
+    # 进行推送
+    push(content)
+except:
+    content = '签到失败'
+    print(content)
+    push(content)
